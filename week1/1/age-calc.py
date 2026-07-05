4d7e8a8d063703141c3ba606712774577c9a75fb
swik404 <swiki9000@gmail.com>
2026-07-05 20:53:51 +0545

    age-calc

---
+++ week1/1/age-calc.py
@@ -0,0 +1,9 @@
+birth_year = int(input("Enter your birth year (BS): "))
+
+current_year = int(input("Enter current year (BS): "))
+
+age = current_year - birth_year
+
+
+
+print(f"You are {age} years old in BS.")
